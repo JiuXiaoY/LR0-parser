fabec69044e35f63b433e82518ff745a8afd6d0f
JiuXiaoY <105855187+JiuXiaoY@users.noreply.github.com>
2023-06-03 16:49:22 +0800

    Update test.java

--- LR_Zero/src/test/java/test.java
+++ LR_Zero/src/test/java/test.java
@@ -1,12 +1,1 @@
-import guru.nidi.graphviz.attribute.Rank;
-import guru.nidi.graphviz.model.Factory;
-import guru.nidi.graphviz.model.MutableGraph;
 
-public class test {
-    public void draw(){
-        MutableGraph graph = Factory.mutGraph("表格").setDirected(true)
-                .graphAttrs().add(Rank.dir(Rank.RankDir.LEFT_TO_RIGHT));
-        大学时期的相关编程作业以及成果
-    }
-
-}
